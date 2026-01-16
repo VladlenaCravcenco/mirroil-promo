2e88ee5b2150e2f2980af71c3545fe2a5f99588d
Vladlena <cravcenco97@inbox.ru>
2026-01-16 15:55:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/mirroil-cherry-men" replace />} />
-        <Route path="/mirroil-cherry-men" element={<CherryMan />} />
-        <Route path="/mirroil-nude-allure" element={<NudeAllure />} />
-        <Route path="*" element={<Navigate to="/mirroil-cherry-men" replace />} />
+        <Route path="/" element={<Navigate to="/cherry-men" replace />} />
+        <Route path="/cherry-men" element={<CherryMan />} />
+        <Route path="/nude-allure" element={<NudeAllure />} />
+        <Route path="*" element={<Navigate to="/cherry-men" replace />} />
       </Routes>
     </BrowserRouter>
   );
